47f133356ec1961b4e6e5574e04ad90b066c7814
fousin <andersoncarlos01@hotmail.com>
2025-05-12 06:38:28 -0300

    A05 - aula 5

--- src/main/java/dev/fousin/CadastrosDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/fousin/CadastrosDeNinjas/Missoes/MissaoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class MissaoController {
-    @GetMapping("/")
+    @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Hello, World!";
     }
